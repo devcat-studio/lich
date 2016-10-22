da1ce9d681160528c813a96c716778f86e9403b0
Seungjae Lee <distynova@gmail.com>
2016-10-22 18:05:26 +0900

    자리 옮김

--- lich/xpcall.h
+++ lich/xpcall.h
@@ -3,10 +3,24 @@
 #include "to.h"
 #include "top_guard.h"
 #include <functional>
+#include <tuple>
 
 namespace lich
 {
 	int error_handler(lua_State* L);
+
+	typename<int I, int LEFT>
+	inline void push_tuple_element()
+	{
+
+	}
+
+	template<typename T>
+	inline void push_tuple(lua_State* L, const T& t)
+	{
+		
+
+	}
 
 	template<typename ARG_TUPLE, typename RET_TUPLE>
 	void xpcall(
@@ -26,10 +40,10 @@ namespace lich
 		lua_pushcclosure(L, &error_handler, 1);
 		int errorHandlerIdx = lua_gettop(L);
 		lua_pushvalue(L, funcIdx);
-		pushv(L, args);
+		push_tuple(L, args);
 		if (lua_pcall(L, tuple_size<args>(), tuple_size<rets>(), errorHandlerIdx) == 0)
 		{
-			tov(L, lua_top() - tuple_size<rets>()  + 1, rets);
+			to_tuple(L, lua_top() - tuple_size<rets>()  + 1, rets);
 		}
 		return ret;
 	}
